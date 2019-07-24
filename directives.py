c8bee99b29eb5227599a20041b3bbeffa07a8185
Max Snippe <m.j.w.snippe@saxion.nl>
2019-07-24 11:45:04 +0200

    Initial commit

---
+++ directives.py
@@ -0,0 +1,329 @@
+import re
+
+from docutils import nodes
+from docutils.parsers.rst import directives, Directive
+from sphinx import addnodes
+from sphinx.directives import ObjectDescription
+from sphinx.util.docfields import Field, TypedField
+
+
+ros_sig_re = re.compile(
+    r'''^((?:[^\.]*\.)*?)         # package name
+         (?:(msg|srv|action)\.)?  # object type
+         (\w+) \s*?$              # thing name
+     ''', re.VERBOSE)
+
+
+# This override allows our inline type specifiers to behave like :class: link
+# when it comes to handling "." and "~" prefixes.
+class RosXRefMixin(object):
+    def make_xref(self, rolename, domain, target, innernode=nodes.emphasis,
+                  contnode=None):
+        result = super(RosXRefMixin, self).make_xref(rolename, domain, target,
+                                                     innernode, contnode)
+        result['refspecific'] = True
+        if target.startswith(('.', '~')):
+            prefix, result['reftarget'] = target[0], target[1:]
+            if prefix == '.':
+                text = target[1:]
+            elif prefix == '~':
+                # text = target.split('.')[-1]
+                text = re.split(r'[\./]', target)[-1]
+            for node in result.traverse(nodes.Text):
+                node.parent[node.parent.index(node)] = nodes.Text(text)
+                break
+        return result
+
+
+class RosObject(ObjectDescription):
+    """
+    Description of a general ROS object.
+    """
+    option_spec = {
+        'noindex': directives.flag,
+        'package': directives.unchanged,
+    }
+
+    def get_signature_prefix(self, sig):
+        """
+        May return a prefix to put before the object name in the signature.
+        """
+        return ''
+
+    def get_object_type_prefix(self):
+        """
+        May return an optional name prefix that defines object type, e.g.
+        'msg' or 'srv'.
+        """
+        return ''
+
+    def handle_signature(self, sig, signode):
+        """
+        Transform a ROS signature into rST nodes.
+
+        Return (fully qualified name of the thing, package name if any).
+
+        If inside a package, the current package name is handled intelligently:
+        * it is stripped from the displayed name if present
+        * it is added to the full name (return value) if not present
+        """
+        m = ros_sig_re.match(sig)
+        if m is None:
+            raise ValueError
+        name_prefix, obj_type, name = m.groups()
+        name_prefix = name_prefix.rstrip('.')
+
+        pkgname = self.options.get(
+            'package', self.env.ref_context.get('ros:package'))
+        if not obj_type:
+            obj_type = self.get_object_type_prefix()
+        if name_prefix and name_prefix.startswith(pkgname):
+            fullname = '.'.join([name_prefix, obj_type, name])
+            name_prefix = name_prefix[len(pkgname):].lstrip('.')
+        elif name_prefix:
+            fullname = '.'.join([pkgname, name_prefix, obj_type, name])
+        else:
+            fullname = '.'.join([pkgname, obj_type, name])
+
+        signode['package'] = pkgname
+        signode['fullname'] = fullname
+
+        sig_prefix = self.get_signature_prefix(sig)
+        if sig_prefix:
+            signode += addnodes.desc_annotation(sig_prefix, sig_prefix)
+
+        if name_prefix:
+            signode += addnodes.desc_addname(name_prefix, name_prefix)
+        elif self.env.config.ros_add_package_names:
+            if pkgname:
+                nodetext = pkgname + '.'
+                if obj_type:
+                    nodetext += obj_type + '.'
+                signode += addnodes.desc_addname(nodetext, nodetext)
+
+        signode += addnodes.desc_name(name, name)
+        return fullname, name_prefix, obj_type
+
+    def get_index_text(self, pkgname, name):
+        """
+        Return the text for the index entry of the object.
+        """
+        raise NotImplementedError('must be implemented in subclasses')
+
+    def add_object_to_domain_data(self, fullname, obj_type):
+        """
+        Add the object to the object lists of the ROS domain data.
+        """
+        raise NotImplementedError('must be implemented in subclasses')
+
+    def add_target_and_index(self, name, sig, signode):
+        pkgname = self.options.get('package',
+                                   self.env.ref_context.get('ros:package'))
+
+        fullname = name[0]
+        obj_type = name[2]
+        # Note target
+        if fullname not in self.state.document.ids:
+            signode['names'].append(fullname)
+            signode['ids'].append(fullname)
+            signode['first'] = (not self.names)
+            self.state.document.note_explicit_target(signode)
+
+            self.add_object_to_domain_data(fullname, obj_type)
+
+            # objects = self.env.domaindata['ros']['objects']
+            # if fullname in objects:
+            #     self.state_machine.reporter.warning(
+            #         'duplicate object description of %s, ' % fullname +
+            #         'other instance in ' +
+            #         self.env.doc2path(objects[fullname][0]) +
+            #         ', use :noindex: for one of them',
+            #         line=self.lineno)
+            # objects[fullname] = (self.env.docname, self.objtype)
+
+            indextext = self.get_index_text(pkgname, name)
+            if indextext:
+                self.indexnode['entries'].append(('single', indextext,
+                                                  fullname, ''))
+
+
+class RosField(RosXRefMixin, Field):
+    pass
+
+
+class RosTypedField(RosXRefMixin, TypedField):
+    pass
+
+
+class RosType(RosObject):
+    """
+    Super class for messages, services, and actions.
+    """
+
+    def get_object_type_prefix(self):
+        if self.objtype == 'message':
+            return 'msg'
+        elif self.objtype == 'service':
+            return 'srv'
+        elif self.objtype == 'action':
+            return 'action'
+
+    def get_signature_prefix(self, sig):
+        return self.objtype + ' '
+
+    def get_index_text(self, pkgname, name):
+        fullname = name[0]
+        obj_type = name[2]
+        if pkgname:
+            if fullname.startswith(pkgname):
+                newname = re.sub(r'^' + pkgname + r'\.', '', fullname)
+                if newname.startswith(obj_type):
+                    newname = re.sub(r'^' + obj_type + r'\.', '', newname)
+            return '{} ({} in package {})'.format(newname, self.objtype,
+                                                  pkgname)
+        else:
+            return '{} ({})'.format(fullname, self.objtype)
+
+    def add_object_to_domain_data(self, fullname, obj_type):
+        objects = self.env.domaindata['ros']['objects']
+        if fullname in objects:
+            self.state_machine.reporter.warning(
+                'duplicate object description of %s, ' % fullname +
+                'other instance in ' +
+                self.env.doc2path(objects[fullname][0]) +
+                ', use :noindex: for one of them',
+                line=self.lineno)
+        objects[fullname] = (self.env.docname, self.objtype)
+
+
+class RosCurrentPackageDirective(Directive):
+    """
+    This directive is just to tell Sphinx that we're documenting stuff in this
+    package, but links to this package will not lead here.
+    """
+
+    has_content = False
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+    option_spec = {}
+
+    def run(self):
+        env = self.state.document.settings.env
+        pkgname = self.arguments[0].strip()
+        if pkgname == 'None':
+            env.ref_context.pop('ros:package', None)
+        else:
+            env.ref_context['ros:package'] = pkgname
+        return []
+
+
+class RosPackageDirective(Directive):
+    """
+    Directive to mark description of a new package.
+    """
+
+    has_content = False
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+    option_spec = {
+        'noindex': directives.flag,
+        'deprecated': directives.flag
+    }
+
+    def run(self):
+        env = self.state.document.settings.env
+        pkgname = self.arguments[0].strip()
+        noindex = 'noindex' in self.options
+        env.ref_context['ros:package'] = pkgname
+        ret = []
+        if not noindex:
+            ros_domain = env.get_domain('ros')
+            ros_domain.add_package(pkgname, 'deprecated' in self.options)
+            # env.domaindata['ros']['packages'][pkgname] = \
+            #     (env.docname, 'deprecated' in self.options)
+            # env.domaindata['ros']['objects'][pkgname] = \
+            #     (env.docname, 'package')
+            targetnode = nodes.target('', '', ids=['package-' + pkgname])
+            self.state.document.note_explicit_target(targetnode)
+            ret.append(targetnode)
+            indextext = '{} (package)'.format(pkgname)
+            inode = addnodes.index(entries=[('single', indextext,
+                                             'package-' + pkgname, '')])
+            ret.append(inode)
+        return ret
+
+
+class RosActionDirective(RosType):
+    """
+    Description of a ROS action type.
+    """
+
+    doc_field_types = [
+        RosTypedField('goal_parameter',
+                      label='Goal parameters',
+                      names=('goal_param',),
+                      typerolename='obj',
+                      typenames=('goal_paramtype',),
+                      can_collapse=True),
+        RosTypedField('result_parameter',
+                      label='Result parameters',
+                      names=('result_param',),
+                      typerolename='obj',
+                      typenames=('result_paramtype',),
+                      can_collapse=True),
+        RosTypedField('feedback_parameter',
+                      label='Feedback parameters',
+                      names=('feedback_param',),
+                      typerolename='obj',
+                      typenames=('feedback_paramtype',),
+                      can_collapse=True)
+    ]
+
+
+class RosServiceDirective(RosType):
+    """
+    Description of a ROS service type.
+    """
+
+    doc_field_types = [
+        RosTypedField('req_parameter',
+                      label='Request parameters',
+                      names=('req_param',),
+                      typerolename='obj',
+                      typenames=('req_paramtype',),
+                      can_collapse=True),
+        RosTypedField('resp_parameter',
+                      label='Response parameters',
+                      names=('resp_param',),
+                      typerolename='obj',
+                      typenames=('resp_paramtype',),
+                      can_collapse=True)
+    ]
+
+
+class RosMessageDirective(RosType):
+    """
+    Description of a ROS message type.
+    """
+
+    option_spec = {
+        'noindex': directives.flag,
+        'package': directives.unchanged,
+        'deprecated': directives.flag,
+    }
+    # option_spec['deprecated'] = directives.flag
+
+    doc_field_types = [
+        RosTypedField('parameter',
+                      label='Parameters',
+                      names=('msg_param',),
+                      typerolename='obj',
+                      typenames=('msg_paramtype',),
+                      can_collapse=True),
+    ]
+
+    def add_object_to_domain_data(self, fullname, obj_type):
+        ros_domain = self.env.get_domain('ros')
+        ros_domain.add_message(fullname, 'deprecated' in self.options)
---
+++ domain.py
@@ -0,0 +1,163 @@
+from six import iteritems
+from docutils import nodes
+from sphinx.domains import Domain, ObjType
+from sphinx.util.nodes import make_refnode
+from .xref_role import RosXRefRole
+from .directives import RosPackageDirective, RosCurrentPackageDirective, \
+    RosMessageDirective, RosActionDirective, RosServiceDirective
+from .indices import RosPackageIndex, RosMessageIndex
+
+
+class RosDomain(Domain):
+    name = 'ros'
+    label = 'ROS'
+    object_types = {
+        'message':   ObjType('message', 'msg', 'obj'),
+        'service':   ObjType('service', 'srv', 'obj'),
+        'action':    ObjType('action', 'action', 'obj')
+    }
+    roles = {
+        'pkg': RosXRefRole(),
+        'msg': RosXRefRole(),
+        'srv': RosXRefRole(),
+        'action': RosXRefRole()
+    }
+    directives = {
+        'package':          RosPackageDirective,
+        'currentpackage':   RosCurrentPackageDirective,
+        'message':          RosMessageDirective,
+        'service':          RosServiceDirective,
+        'action':           RosActionDirective
+    }
+    initial_data = {
+        'objects': {},   # fullname -> docname, objtype
+        'packages': {},  # name -> document name, anchor, priority, deprecated
+        'messages': {},  # name -> document name, anchor, priority, deprecated
+        'labels': {
+            'ros-pkgindex': ('ros-pkgindex', '', 'Package Index'),
+            'ros-msgindex': ('ros-msgindex', '', 'Message Type Index')
+        },
+        'anonlabels': {
+            'ros-pkgindex': ('ros-pkgindex', ''),
+            'ros-msgindex': ('ros-msgindex', '')
+        }
+    }
+    indices = [
+        RosPackageIndex,
+        RosMessageIndex,
+    ]
+
+    def clear_doc(self, docname):
+        for fullname, (fn, _l) in list(self.data['objects'].items()):
+            if fn == docname:
+                del self.data['objects'][fullname]
+        # name -> document name, anchor, priority, deprecated
+        for pkgname, (fn, _, _, _) in list(self.data['packages'].items()):
+            if fn == docname:
+                del self.data['packages'][pkgname]
+
+    def find_obj(self, env, pkgname, name, type, searchmode=0):
+        """
+        Find a ROS object for ``name``, perhaps using ``pkgname``.
+        """
+        if name.endswith('()'):
+            name = name[:-2]
+
+        if not name:
+            return []
+
+        objects = self.data['objects']
+        matches = []
+
+        newname = None
+        if searchmode == 1:
+            if type is None:
+                objtypes = list(self.object_types)
+            else:
+                objtypes = self.objtypes_for_role(type)
+        else:
+            # NOTE: searching for exact match, object type is not considered
+            if name in objects:
+                newname = name
+            elif type == 'pkg':
+                # only exact matches allowed for packages
+                return []
+            elif pkgname and pkgname + '.' + name in objects:
+                newname = pkgname + '.' + name
+
+        if newname is not None:
+            matches.append((newname, objects[newname]))
+        return matches
+
+    def resolve_xref(self, env, fromdocname, builder, type, target, node,
+                     contnode):
+        pkgname = node.get('ros:package')
+        searchmode = node.hasattr('refspecific') and 1 or 0
+
+        matches = self.find_obj(env, pkgname, target, type, searchmode)
+
+        if not matches:
+            return None
+        elif len(matches) > 1:
+            env.warn_node(
+                'more than one target found for cross-reference '
+                '%r: %s' % (target, ', '.join(match[0] for match in matches)),
+                node)
+        name, obj = matches[0]
+
+        if obj[1] == 'package':
+            return self._make_package_refnode(builder, fromdocname, name,
+                                              contnode)
+        else:
+            return make_refnode(builder, fromdocname, obj[0], name, contnode,
+                                name)
+
+    def resolve_any_xref(self, env, fromdocname, builder, target, node,
+                         contnode):
+        pkgname = node.get('ros:package')
+        results = []
+
+        # Always search in 'refspecific' mode with the :any: role
+        matches = self.find_obj(env, pkgname, target, None, 1)
+        for name, obj in matches:
+            if obj[1] == 'package':
+                results.append(('ros:pkg',
+                               self._make_package_refnode(builder,
+                                                          fromdocname,
+                                                          name, contnode)))
+            else:
+                results.append(('ros:' + self.role_for_objtype(obj[1]),
+                                make_refnode(builder, fromdocname, obj[0],
+                                             name, contnode, name)))
+        return results
+
+    def _make_package_refnode(self, builder, fromdocname, name, contnode):
+        # Get additional info for packages
+        # name -> document name, anchor, priority, deprecated
+        docname, anchor, _, deprecated = self.data['packages'][name]
+        title = name
+        if deprecated:
+            title += ' (deprecated)'
+        return make_refnode(builder, fromdocname, docname, anchor, contnode,
+                            title)
+
+    def get_objects(self):
+        for pkgname, info in iteritems(self.data['packages']):
+            yield (pkgname, pkgname, 'package', info[0],
+                   'package-' + pkgname, 0)
+        for refname, (docname, type) in iteritems(self.data['objects']):
+            if type != 'package':
+                yield (refname, refname, type, docname, refname, 1)
+
+    def add_package(self, name, deprecated):
+        anchor = 'ros-pkg-{}'.format(name)
+        # name -> document name, anchor, priority, deprecated
+        self.data['packages'][name] = (self.env.docname, anchor, 0, deprecated)
+        # make a duplicate entry in 'objects' to facilitate searching for the
+        # package in RosDomain.find_obj()
+        self.data['objects'][name] = (self.env.docname, 'package')
+
+    def add_message(self, name, deprecated):
+        # name -> document name, anchor, priority, deprecated
+        self.data['messages'][name] = (self.env.docname, name, 0, deprecated)
+        self.data['objects'][name] = (self.env.docname, 'message')
